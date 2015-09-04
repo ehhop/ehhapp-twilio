9feffc3e3d4748c04052c99b2635ac54cd1ff8cb
Ryan Neff <raneff@gmail.com>
2015-09-03 21:00:43 -0400

    with s3

--- hello.py
+++ hello.py
@@ -1,28 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, request, send_from_directory
 import twilio.twiml
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='/home/neffra/test_twilio/recordings')
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_monkey():
-	'''
-	respond to incoming requests:
+	'''respond to incoming requests:'''
 	
-	Gracias por llamar a la Clínica de EHHOP. 
-	Para instrucciones en español, marque el número 2. 
-	Thank you for calling the EHHOP clinic. 
-	For instructions in English, please press 1. 
-	EHHOP es la Asociación Comunitaria para la Salud de 
-	East Harlem en Mount Sinai. Si esto es una emergencia, 
-	cuelgue el teléfono y llame ahora al 9-1-1. We are the 
-	East Harlem Health Outreach Partnership of the Icahn School 
-	of Medicine at Mount Sinai. If this is an emergency please 
-	hang up and dial 9 1 1 now.
-	'''
-	
+	#Gracias por llamar a la Clinica de EHHOP. 
+	#Para instrucciones en espanol, marque el numero 2. 
+	#Thank you for calling the EHHOP clinic. 
+	#For instructions in English, please press 1. 
+	#EHHOP es la Asociacion Comunitaria para la Salud de 
+	#East Harlem en Mount Sinai. Si esto es una emergencia, 
+	#cuelgue el telefono y llame ahora al 9-1-1. We are the 
+	#East Harlem Health Outreach Partnership of the Icahn School 
+	#of Medicine at Mount Sinai. If this is an emergency please 
+	#hang up and dial 9 1 1 now.
 
 	resp = twilio.twiml.Response()
-	resp.play("recordings/welcome_greeting.mp3")
+	resp.play("https://s3.amazonaws.com/ehhapp-phone/welcome_greeting_ehhop.mp3")
 
 	return str(resp)
