f98cb87d64ada0e215479069fef83c0ccff692fc
Ryan Neff <raneff@gmail.com>
2015-09-03 20:34:23 -0400

    with mp3

--- hello.py
+++ hello.py
@@ -6,16 +6,23 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_monkey():
-	'''respond to incoming requests'''
-
-	welcome_greeting = \
-	"Thank you for calling the e hop clinic." + \
-	" If this is a medical emergency please hang up and dial 9 1 1." + \
-	" For instructions in English, please press 1." + \
-	" We are the East Harlem Health Outreach Partnership of the Icahn" + \
-	" School of Medicine at Mount Sinai. "
+	'''
+	respond to incoming requests:
+	
+	Gracias por llamar a la Clínica de EHHOP. 
+	Para instrucciones en español, marque el número 2. 
+	Thank you for calling the EHHOP clinic. 
+	For instructions in English, please press 1. 
+	EHHOP es la Asociación Comunitaria para la Salud de 
+	East Harlem en Mount Sinai. Si esto es una emergencia, 
+	cuelgue el teléfono y llame ahora al 9-1-1. We are the 
+	East Harlem Health Outreach Partnership of the Icahn School 
+	of Medicine at Mount Sinai. If this is an emergency please 
+	hang up and dial 9 1 1 now.
+	'''
+	
 
 	resp = twilio.twiml.Response()
-	resp.say(welcome_greeting, voice="woman", language="en", loop=3)
+	resp.play("recordings/welcome_greeting.mp3")
 
 	return str(resp)
