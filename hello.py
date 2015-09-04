882c2c3d761561620390332b3099eff80e807b10
Ryan Neff <raneff@gmail.com>
2015-09-03 20:19:27 -0400

    test ehhop

--- hello.py
+++ hello.py
@@ -1,8 +1,20 @@
 import os
 from flask import Flask
+import twilio.twiml
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def hello_monkey():
+	'''respond to incoming requests'''
+
+	welcome_greeting = "Thank you for calling the e hop clinic." + \
+	"If this is a medical emergency please hang up and dial 9 1 1." + \
+	"For instructions in English, please press 1." + \
+	"We are the East Harlem Health Outreach Partnership of the Icahn " + \
+	"School of Medicine at Mount Sinai. "
+
+	resp = twilio.twiml.Response()
+	resp.say(welcome_greeting, voice="woman", language="en", loop=3)
+
+	return str(resp)
