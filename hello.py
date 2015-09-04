54d0d3e89b271af62d0a6eb4e008151e5016fb14
Ryan Neff <raneff@gmail.com>
2015-09-03 20:22:29 -0400

    test ehhop 2

--- hello.py
+++ hello.py
@@ -8,11 +8,12 @@ app = Flask(__name__)
 def hello_monkey():
 	'''respond to incoming requests'''
 
-	welcome_greeting = "Thank you for calling the e hop clinic." + \
-	"If this is a medical emergency please hang up and dial 9 1 1." + \
-	"For instructions in English, please press 1." + \
-	"We are the East Harlem Health Outreach Partnership of the Icahn " + \
-	"School of Medicine at Mount Sinai. "
+	welcome_greeting = \
+	"Thank you for calling the e hop clinic." + \
+	" If this is a medical emergency please hang up and dial 9 1 1." + \
+	" For instructions in English, please press 1." + \
+	" We are the East Harlem Health Outreach Partnership of the Icahn" + \
+	" School of Medicine at Mount Sinai. "
 
 	resp = twilio.twiml.Response()
 	resp.say(welcome_greeting, voice="woman", language="en", loop=3)
