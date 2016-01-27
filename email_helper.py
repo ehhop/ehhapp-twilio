ac8d5db8e9422dc6e596e8fb993bd5d841b4590b
Ryan Neff <rneff@ehhapp.org>
2016-01-27 00:11:22 -0500

    Bugfix #2.

--- email_helper.py
+++ email_helper.py
@@ -1,4 +1,5 @@
-import pytz, smtplib, requests, shutil, random, string, re, os, ftplib
+import pytz, smtplib, requests, shutil, random, string, re, os
+from ftplib import FTP_TLS
 from datetime import datetime, timedelta
 from email.mime.text import MIMEText
 from requests.auth import HTTPBasicAuth
@@ -59,7 +60,7 @@ def save_file(recording_url, auth_method):
 def save_file_with_name(recording_url, auth_method, save_name):
 	# we are now using HTTP basic auth to do the downloads
 	# step 0 - open up a FTP session with HIPAA box
-	session = ftplib.FTP('ftp.box.com', box_username, box_password)
+	session = FTP_TLS('ftp.box.com', box_username, box_password)
 	# step 1 - open a request to get the voicemail using a secure channel with Twilio
 	response = requests.get(recording_url, stream=True, auth=auth_method) # no data has been downloaded yet (just headers)
 	# step 2 - read the response object in chunks and write it to the HIPAA box directly
