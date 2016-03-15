cbb8c41ca9496a3691ae28df106848fcf5a91810
Ryan Neff <rneff@ehhapp.org>
2016-03-15 19:26:30 -0400

    Moved config file to HIPAA storage and backing up the encrypted DB to HIPAA storage.

---
+++ db_backup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys, os
+import site
+site.addsitedir('/var/wsgiapps/ehhapp_twilio/venv/lib/python2.7/site-packages')
+
+PROJECT_DIR = '/var/wsgiapps/ehhapp_twilio/'
+sys.path.append(PROJECT_DIR)
+sys.path.append(PROJECT_DIR + '/venv')
+
+activate_this = os.path.expanduser('/var/wsgiapps/ehhapp_twilio/venv/bin/activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
+
+from ehhapp_twilio.config import *
+from ftplib import FTP_TLS
+import subprocess
+
+save_name = subprocess.check_output('date +%m%d%y', shell=True).strip('\n') + '-ehhapp-twilio.db'
+session = FTP_TLS('ftp.box.com', box_username, box_password)
+session.storbinary('STOR server/' + save_name, open('ehhapp-twilio.db', 'rb'))
+session.quit()
