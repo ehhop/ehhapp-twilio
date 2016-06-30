ceee1b2053157dba0e02b502aee50c208b0b9e33
Ryan Neff <rneff@ehhapp.org>
2016-06-30 09:22:53 -0400

    Remove default channel from webhooks, remove output from clean command.

--- ehhapp_twilio/webhooks.py
+++ ehhapp_twilio/webhooks.py
@@ -4,8 +4,8 @@ from ehhapp_twilio.config import *
 def slack_notify(msg, uname=slack_user, icon=slack_icon, cn=slack_channel, hookurl = hookurl):
 	payload = {'text': msg, 
 		'username': uname, 
-		'icon_emogi':icon, 
-		'channel': cn}
+		'icon_emogi':icon} 
+#		'channel': 'ehhop-voicemails'}
 	payload_json = json.dumps(payload)
 	requests.post(hookurl, data=payload_json)
 	return None
